4ac88b0d301a07eb14818ebce905df6600d12a2f
Olatoyan <golatoyan@gmail.com>
2023-05-26 18:06:52 +0100

    new commit

--- starter-code/script.js
+++ starter-code/script.js
@@ -11,19 +11,7 @@ const destinationSection = document.querySelector(".destination__section");
 const crewSection = document.querySelector(".crew__section");
 const technologySection = document.querySelector(".technology__section");
 const destinationLinks = document.querySelector(".destinations");
-
-// navLinks.addEventListener("click", function (e) {
-//   // console.log(e.target);
-//   if (e.target.closest("li").querySelector("#home")) {
-//     // if (e.target.closest("li").querySelector("#home")) {
-//     // console.log(e.target.querySelector("#home"));
-//     // console.log(e.target.querySelector(".active__link"));
-//     // console.log(e.target.querySelector("id"));
-//     console.log(e.target);
-//     console.log("qq");
-//     // }
-//   }
-// });
+const dotContainer = document.querySelector(".dots");
 
 navLinks.addEventListener("click", function (e) {
   e.preventDefault();
@@ -188,6 +176,74 @@ exploreBtn.addEventListener("click", function (e) {
 //     }
 //   });
 // };
+let r;
+// const renderDestination = function (data) {
+//   const html = `
+//           <div class="destination__box">
+//               <div class="tertiary__box">
+//                 <span>01</span>
+//                 <h3 class="tertiary__heading">Pick your destination</h3>
+//               </div>
+//             <div class="destination__img-box">
+//               <img
+//                 src="${data.images.png}"
+//                 alt="space image"
+//                 class="destination__img"
+//               />
+//             </div>
+//           </div>
+//           <div class="destination__text-box">
+//             <div class="destinations__box">
+//               <ul class="destinations">
+//                 <li>
+//                   <a href="#" class=" destination moon">Moon</a>
+//                 </li>
+//                 <li><a href="#" class="destination mars">Mars</a></li>
+//                 <li><a href="#" class="destination europa">Europa</a></li>
+//                 <li><a href="#" class="destination titan">Titan</a></li>
+//               </ul>
+//             </div>
+//             <h2 class="destination__heading">${data.name}</h2>
+//             <p class="destination__text">${data.description}</p>
+//             <div class="destination__distance">
+//               <div class="distance__box">
+//                 <p class="distance__text">Avg. distance</p>
+//                 <p class="distance">${data.distance}</p>
+//               </div>
+//               <div class="travel__box">
+//                 <p class="travel__text">Est. travel time</p>
+//                 <p class="travel__duration">${data.travel}</p>
+//               </div>
+//             </div>
+//             <!-- </div> -->
+//           </div>
+// `;
+
+//   destinationSection.textContent = "";
+//   destinationSection.insertAdjacentHTML("beforeend", html);
+
+//   // const destinationLinks = document.querySelector(".destinations");
+//   // const removeDesClass = function () {
+//   //   const activeDes = document.querySelectorAll(".active__destination");
+//   //   activeDes.forEach(function (link) {
+//   //     link.classList.remove("active__destination");
+//   //   });
+//   // };
+//   // destinationLinks.addEventListener("click", function (e) {
+//   //   e.preventDefault();
+//   //   removeDesClass();
+
+//   // if (
+//   //   e.target.classList.contains("moon") ||
+//   //   e.target.classList.contains("mars") ||
+//   //   e.target.classList.contains("europa") ||
+//   //   e.target.classList.contains("titan")
+//   // ) {
+//   //   e.target.classList.add("active__destination");
+//   // }
+//   //   //   //   // console.log(renderDestination());
+//   // });
+// };
 
 const renderDestination = function (data) {
   const html = `
@@ -199,7 +255,7 @@ const renderDestination = function (data) {
             <div class="destination__img-box">
               <img
                 src="${data.images.png}"
-                alt="Moon image"
+                alt="space image"
                 class="destination__img"
               />
             </div>
@@ -207,12 +263,11 @@ const renderDestination = function (data) {
           <div class="destination__text-box">
             <div class="destinations__box">
               <ul class="destinations">
-                <li>
-                  <a href="#" class=" destination moon">Moon</a>
+                <li class="destination moon">Moon
                 </li>
-                <li><a href="#" class="destination mars">Mars</a></li>
-                <li><a href="#" class="destination europa">Europa</a></li>
-                <li><a href="#" class="destination titan">Titan</a></li>
+                <li class="destination mars">Mars</li>
+                <li class="destination europa">Europa</li>
+                <li class="destination titan">Titan</li>
               </ul>
             </div>
             <h2 class="destination__heading">${data.name}</h2>
@@ -233,36 +288,51 @@ const renderDestination = function (data) {
 
   destinationSection.textContent = "";
   destinationSection.insertAdjacentHTML("beforeend", html);
-
-  // const destinationLinks = document.querySelector(".destinations");
-  // const removeDesClass = function () {
-  //   const activeDes = document.querySelectorAll(".active__destination");
-  //   activeDes.forEach(function (link) {
-  //     link.classList.remove("active__destination");
-  //   });
-  // };
-  // destinationLinks.addEventListener("click", function (e) {
-  //   e.preventDefault();
-  //   removeDesClass();
-
-  // if (
-  //   e.target.classList.contains("moon") ||
-  //   e.target.classList.contains("mars") ||
-  //   e.target.classList.contains("europa") ||
-  //   e.target.classList.contains("titan")
-  // ) {
-  //   e.target.classList.add("active__destination");
-  // }
-  //   //   //   // console.log(renderDestination());
-  // });
 };
+const renderCrew = function (data, activeDotIndex) {
+  const html = `
+    <div class="crew__box">
+      <div class="tertiary__box">
+        <span>02</span>
+        <h3 class="tertiary__heading">Meet your crew</h3>
+      </div>
+      <div class="crew__text-box">
+        <h3 class="crew__role">${data.role}</h3>
+        <h2 class="crew__name">${data.name}</h2>
+        <p class="crew__bio">${data.bio}</p>
+      </div>
+      <div class="dots">
+        <div class="dot dot-1 data-dot-index${
+          activeDotIndex === 1 ? " active__dot" : ""
+        }"></div>
+        <div class="dot dot-2 data-dot-index${
+          activeDotIndex === 2 ? " active__dot" : ""
+        }"></div>
+        <div class="dot dot-3 data-dot-index${
+          activeDotIndex === 3 ? " active__dot" : ""
+        }"></div>
+        <div class="dot dot-4 data-dot-index${
+          activeDotIndex === 4 ? " active__dot" : ""
+        }"></div>
+      </div>
+    </div>
+    <div class="crew__img-box">
+      <img src="${data.images.png}" alt="" class="crew__img" />
+    </div>
+  `;
+
+  crewSection.insertAdjacentHTML("beforeend", html);
+};
+
 const destination = async function () {
   const res = await fetch("data.json");
   const data = await res.json();
   console.log(data);
   const destinationSection = document.querySelector(".destination__section");
+  const crewSection = document.querySelector(".crew__section");
   renderDestination(data.destinations[0]);
-  console.log(data.destinations[0]);
+  renderCrew(data.crew[0], 1);
+  console.log(data.crew[0]);
 
   destinationSection.addEventListener("click", function (e) {
     if (e.target.classList.contains("destination")) {
@@ -291,54 +361,93 @@ const destination = async function () {
       }
     }
   });
+
+  // crewSection.addEventListener("click", function (e) {
+  //   e.preventDefault();
+  //   const dots = crewSection.querySelectorAll(".dot");
+  //   dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+  //   const dotClass = e.target.classList;
+  //   if (
+  //     dotClass.contains("dot-1") ||
+  //     dotClass.contains("dot-2") ||
+  //     dotClass.contains("dot-3") ||
+  //     dotClass.contains("dot-4")
+  //   ) {
+  //     const dots = crewSection.querySelectorAll(".dot");
+
+  //     crewSection.textContent = "";
+  //     const dotNumber = dotClass[1].charAt(4) - 1;
+  //     // renderCrew(data.crew[dotNumber]);
+  //     const dotElement = e.target.closest(".dot");
+  //     if (dotElement) {
+  //       const dotIndex = parseInt(dotElement.classList[1].split("-")[1]);
+  //       renderCrew(data.crew[dotIndex - 1], true);
+
+  //       dotElement.classList.add("active__dot");
+  //     }
+  //     console.log("a");
+  //     dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+  //     dotClass.add("active__dot");
+  //   }
+  // });
+  let r;
+  // crewSection.addEventListener("click", function (e) {
+  //   e.preventDefault();
+  //   const dots = crewSection.querySelectorAll(".dot");
+  //   dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+  //   const dotClass = e.target.classList;
+  //   if (
+  //     dotClass.contains("dot-1") ||
+  //     dotClass.contains("dot-2") ||
+  //     dotClass.contains("dot-3") ||
+  //     dotClass.contains("dot-4")
+  //   ) {
+  //     const dots = crewSection.querySelectorAll(".dot");
+
+  //     crewSection.textContent = "";
+  //     const dotNumber = dotClass[1].charAt(4) - 1;
+
+  //     dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+  //     renderCrew(data.crew[dotNumber], true);
+  //     console.log("a");
+
+  //     dotClass.add("active__dot");
+  //   }
+  // });
+
+  crewSection.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("dot") && !e.target.closest(".dot")) {
+      return;
+    }
+    e.preventDefault();
+    const dots = crewSection.querySelectorAll(".dot");
+    dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+    const dotClass = e.target.classList;
+    if (
+      dotClass.contains("dot-1") ||
+      dotClass.contains("dot-2") ||
+      dotClass.contains("dot-3") ||
+      dotClass.contains("dot-4")
+    ) {
+      const dots = crewSection.querySelectorAll(".dot");
+
+      crewSection.textContent = "";
+      const dotNumber = dotClass[1].charAt(4) - 1;
+
+      dots.forEach((dot) => dot.classList.remove("active__dot"));
+
+      renderCrew(data.crew[dotNumber], dotNumber + 1);
+      console.log("a");
+
+      dotClass.add("active__dot");
+    } else {
+      return;
+    }
+  });
 };
 destination();
-
-destinationSection.addEventListener("click", function (e) {
-  if (e.target.classList.contains("destination")) {
-    e.preventDefault();
-  }
-});
-// const destination = async function () {
-//   const res = await fetch("data.json");
-//   const data = await res.json();
-//   console.log(data);
-//   renderDestination(data.destinations[1]);
-//   console.log(data.destinations[0]);
-//   const destinationSection = document.querySelector(".destination__section");
-
-//   destinationSection.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     const destinationLinks = document.querySelector(".destinations");
-//     const removeDesClass = function () {
-//       const activeDes = document.querySelectorAll(".active__destination");
-//       activeDes.forEach(function (link) {
-//         link.classList.remove("active__destination");
-//       });
-//     };
-//     destinationLinks.addEventListener("click", function (e) {
-//       console.log("aa");
-//       e.preventDefault();
-//       removeDesClass();
-//       if (e.target.classList.contains("destination")) {
-//         e.target.classList.add("active__destination");
-//       }
-//       //   // console.log(renderDestination());
-
-//       if (e.target.classList.contains("moon")) {
-//         console.log("Moon clicked");
-//         renderDestination(data.destinations[0]);
-//       } else if (e.target.classList.contains("mars")) {
-//         console.log("Mars clicked");
-//         renderDestination(data.destinations[1]);
-//       } else if (e.target.classList.contains("europa")) {
-//         renderDestination(data.destinations[2]);
-//       } else if (e.target.classList.contains("titan")) {
-//         renderDestination(data.destinations[3]);
-//       } else {
-//         return;
-//       }
-//     });
-//   });
-// };
